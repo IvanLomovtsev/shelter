1141959a9cd3ad3b661f4012bced1d3d3d9d17b6
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2022-12-08 21:14:15 +0500

    feat: install filemanager-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
@@ -12,6 +13,13 @@ module.exports = {
         template: path.join(__dirname, 'src', 'template.html'),
         filename: 'index.html',
         }),
+        new FileManagerPlugin({
+            events: {
+            onStart: {
+                delete: ['dist'],
+            },
+        },
+    }),
     ],
     devServer: {
         watchFiles: path.join(__dirname, 'src'),

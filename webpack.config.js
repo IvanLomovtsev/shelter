4d834be3980f31f0367fb82aa97a2b8ad9b1af1e
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2022-12-08 20:49:24 +0500

    feat: add template.html

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'template.html'),
+      filename: 'index.html',
+    }),
+  ],
+};

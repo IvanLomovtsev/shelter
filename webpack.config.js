8d8df7b55bc65f280a9760689f836ffb6d0d4821
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2022-12-08 21:08:41 +0500

    feat: add entry point and output point

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'template.html'),
-      filename: 'index.html',
-    }),
-  ],
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'template.html'),
+        filename: 'index.html',
+        }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000,
+    },
 };

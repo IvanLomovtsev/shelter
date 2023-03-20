3e7aec8dc7d4524b99d7cab4931b508beb84479b
IvanLomovtsev <i.d.lomovtsev@gmail.com>
2023-03-20 22:54:16 +0500

    feat: add styles and images

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './styles/main.scss';
-
 function component(text) {
     const element = document.createElement('h1');
     element.textContent = text;
     return element;
   }
   
-  document.body.prepend(component('Проект собран на Webpack'));
+document.body.prepend(component('Проект собран на Webpack'));
+
+import './main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,58 @@ const FileManagerPlugin = require('filemanager-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.[contenthash].js',
-        assetModuleFilename: "assets/images/[name].[contenthash][ext]",
-      },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader', 
-                    'postcss-loader', 
-                    'sass-loader'
-                ],
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/fonts/[name].[contenthash][ext]',
-                },
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/icons/[name].[contenthash][ext]',
-                },
-            },
-        ],
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.[contenthash].js',
+    assetModuleFilename: 'assets/[name].[contenthash][ext]',
+  },
+  module: {
+    rules: [
+    {
+        test: /\.html$/i,
+        loader: "html-loader",
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'src', 'template.html'),
-        filename: 'index.html',
-        }),
-        new FileManagerPlugin({
-            events: {
+    {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+    },
+    {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+    },
+    {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+    },
+    {
+        test: /\.svg$/,
+        type: 'asset/resource',
+        generator: {
+            filename: 'assets/icons/[name].[contenthash][ext]',
+        },
+    },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+    new FileManagerPlugin({
+        events: {
             onStart: {
                 delete: ['dist'],
-                },
             },
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
-    ],
-    devServer: {
-        watchFiles: path.join(__dirname, 'src'),
-        port: 9000,
+        },
+    }),
+    new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+    }),
+  ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 9000,
     },
 };
